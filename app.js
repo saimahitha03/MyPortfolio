8731fd18a8c6a9d56e4b38be2afdc21fe90afa1e
saimahitha03 <139154894+saimahitha03@users.noreply.github.com>
2024-08-21 12:56:54 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,27 @@
+
+function openWhatsApp(){
+    var phonenumber="6302075690";
+    var message="Hey Hii";
+    var url="https://wa.me/"+phonenumber+"?text="+encodeURIComponent(message);
+    window.open(url,"_blank");
+}
+function insta(){
+    var id="maahii3703";
+    var url="https://instagram.com/"+id;
+    window.open(url,"_blank");
+}
+function fb(){
+    var id="msdhoni";
+    var url="https://facebook.com/"+id;
+    window.open(url,"_blank");
+}
+function linkedIn(){
+    var name="sai-mahitha-gopasani-734650266"
+    var url="https://in.linkedin.com/in/"+name;
+    window.open(url,"_blank");
+}
+function gmail(){
+    var mail="gsaimahitha03@gmail.com"
+    var url="https://mail.google.com/mail/?view=cm&fs=1&to="+mail;
+    window.open(url,"_blank");
+}
